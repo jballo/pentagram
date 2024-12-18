2a5d53435706d41200a622fe8568352e74f58fa3
Jonathan B <31107158+jballo@users.noreply.github.com>
2024-12-17 19:47:45 -0800

    Feat: Modal app is created that generated iamge from 'black-forest-labs/FLUX.1-schnell' model.

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,33 +1,59 @@
 import sys
 
 import modal
+import modal.gpu
 
-app = modal.App("example-hello-world")
+# pip install -U diffusers
+image = (
+    modal.Image.debian_slim(python_version="3.12")
+    .pip_install(
+        "accelerate==0.33.0",
+        "diffusers==0.31.0",
+        "fastapi[standard]==0.115.4",
+        "huggingface-hub[hf_transfer]==0.25.2",
+        "sentencepiece==0.2.0",
+        "torch==2.5.1",
+        "torchvision==0.20.1",
+        "transformers~=4.44.0",
+    )
+    .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})  # faster downloads
+)
 
-@app.function()
-def f(i):
-    if i % 2 == 0:
-        print("hello", i)
-    else:
-        print("world", i, file=sys.stderr)
 
-    return i * i
+app = modal.App(name="example-hello-world", image=image)
 
 
-@app.local_entrypoint()
-def main():
-    # run the function locally
-    print(f.local(1000))
+with image.imports():
+    import io
+    import torch
+    from fastapi import Response
+    from diffusers import FluxPipeline
 
-    # run the function remotely on Modal
-    print(f.remote(1000))
 
-    # run the function in parallel and remotely on Modal
-    total = 0
-    for ret in f.map(range(200)):
-        total += ret
+@app.cls(image=image, gpu="a100", secrets=[modal.Secret.from_name("custom-secret")])
+class Model:
+    @modal.build()
+    @modal.enter()
+    def load_weights(self):
+        # login(token=os.environ["HUGGING_FACE_ACCES_TOKEN"])
+        self.pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16,)
+        self.pipe.to("cuda")
+        self.pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
 
-    print(total)
+    @modal.web_endpoint()
+    def generate(self, prompt="Two russian blue cats holding a sign that says hello world"):
+        image = self.pipe(
+            prompt,
+            guidance_scale=0.0,
+            num_inference_steps=4,
+            max_sequence_length=256,
+            generator=torch.Generator("cpu").manual_seed(0)
+        ).images[0]
+        # image.save("flux-dev.png")
+
+        buffer = io.BytesIO()
+        image.save(buffer, format="JPEG")
+        return Response(content=buffer.getvalue(), media_type="image/jpeg")
 
 
 
