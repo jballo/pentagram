6dbc104c23da839244365f58b5fd4035f8ad484f
Jonathan B <31107158+jballo@users.noreply.github.com>
2024-12-18 17:48:40 -0800

    Enhancement: Improved speed of image generation by switching over to 'stabilityai/stable-diffusion-3.5-large-turbo'. Nextjs server now measures how long it takes to receive generated image from modal server.

--- backend/api/main.py
+++ backend/api/main.py
@@ -25,9 +25,14 @@ app = modal.App(name="example-hello-world", image=image)
 
 with image.imports():
     import io
+    import os
     import torch
     from fastapi import Response
     from diffusers import FluxPipeline
+    from diffusers import StableDiffusionPipeline
+    from diffusers import StableDiffusion3Pipeline
+    from huggingface_hub import login
+
 
 
 @app.cls(image=image, gpu="a100", secrets=[modal.Secret.from_name("custom-secret")])
@@ -35,21 +40,37 @@ class Model:
     @modal.build()
     @modal.enter()
     def load_weights(self):
-        # login(token=os.environ["HUGGING_FACE_ACCES_TOKEN"])
-        self.pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16)
-        self.pipe.to("cuda")
-        self.pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
+        # self.pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16)
+        # self.pipe.to("cuda") # ~40seconds
+        # self.pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
+
+        # self.pipe = StableDiffusionPipeline.from_pretrained("sd-legacy/stable-diffusion-v1-5", torch_dtype=torch.float16)
+        # self.pipe = self.pipe.to("cuda")
+
+        login(token=os.environ["HUGGING_FACE_ACCES_TOKEN"])
+        self.pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3.5-large-turbo", torch_dtype=torch.bfloat16)
+        self.pipe = self.pipe.to("cuda")
+
+
 
     @modal.web_endpoint()
     def generate(self, prompt="Two russian blue cats holding a sign that says hello world"):
+        # image for flux schnell
+        # image = self.pipe(
+        #     prompt,
+        #     guidance_scale=0.0,
+        #     num_inference_steps=4,
+        #     max_sequence_length=256,
+        #     generator=torch.Generator("cpu").manual_seed(0)
+        # ).images[0]
+        # image for stable diffusion 1.5
+        # image = self.pipe(prompt).images[0]
+        # image for stable diffusion 3.5 large turbo
         image = self.pipe(
             prompt,
-            guidance_scale=0.0,
             num_inference_steps=4,
-            max_sequence_length=256,
-            generator=torch.Generator("cpu").manual_seed(0)
+            guidance_scale=0.0,
         ).images[0]
-        # image.save("flux-dev.png")
 
         buffer = io.BytesIO()
         image.save(buffer, format="JPEG")
--- src/app/api/generate-image/route.ts
+++ src/app/api/generate-image/route.ts
@@ -9,8 +9,13 @@ export async function POST(request: Request) {
 
     // TODO: Call your Image Generation API here
     // For now, we'll just echo back the text
+    const start = new Date().getTime();
     const response = await fetch(`https://jballo--example-hello-world-model-generate-dev.modal.run/?prompt=${text}`);
-
+    
+    const elapsed = new Date().getTime() - start;
+    console.log("Time to generate image: ", elapsed, "ms");
+    const timeElapsedSeconds = elapsed / 1000;
+    console.log("Time to generate image: ", timeElapsedSeconds, "s");
     if (!response.ok){
       return NextResponse.json(
         { success: false, error: "Failed to get image"},
@@ -18,7 +23,7 @@ export async function POST(request: Request) {
       );
     }
 
-    console.log(response);
+    // console.log(response);
 
     const blob = await response.blob();
 
